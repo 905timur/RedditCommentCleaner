907dd40f24eb5f7e647a98d5f7d89610079cf909
DaLuSt <daan_lucas@live.nl>
2023-10-13 18:17:27 +0200

    added exception handing and docstrings

--- commentCleaner.py
+++ commentCleaner.py
@@ -1,8 +1,13 @@
 import praw
 import time
 
-
 def get_reddit_credentials():
+    """
+    Prompt the user to input Reddit client credentials.
+
+    Returns:
+        tuple: A tuple containing client_id, client_secret, username, and password.
+    """
     client_id = input("Enter your Reddit client ID: ")
     client_secret = input("Enter your Reddit client secret: ")
     username = input("Enter your Reddit username: ")
@@ -10,6 +15,12 @@ def get_reddit_credentials():
     return client_id, client_secret, username, password
 
 def get_days_old():
+    """
+    Prompt the user to input the age limit for comments.
+
+    Returns:
+        int: The number of days for the age limit.
+    """
     while True:
         days_old = input("Enter how old (in days) the comments should be: ")
         try:
@@ -19,10 +30,28 @@ def get_days_old():
             print("Error: Please enter a number.")
 
 def confirm_and_run():
+    """
+    Ask the user for confirmation to run the script.
+
+    Returns:
+        bool: True if the user confirms, False otherwise.
+    """
     confirmation = input("Do you want to run the script? (yes/no): ")
     return confirmation.lower() == 'yes' or confirmation.lower() == 'y'
 
 def initialize_reddit(client_id, client_secret, username, password):
+    """
+    Initialize the Reddit instance with user credentials.
+
+    Args:
+        client_id (str): Reddit client ID.
+        client_secret (str): Reddit client secret.
+        username (str): Reddit username.
+        password (str): Reddit password.
+
+    Returns:
+        praw.Reddit: An authenticated Reddit instance.
+    """
     try:
         reddit = praw.Reddit(
             client_id=client_id,
@@ -30,34 +59,62 @@ def initialize_reddit(client_id, client_secret, username, password):
             username=username,
             password=password,
             user_agent='commentCleaner',
-            validate_on_submit=True  # Handle the DeprecationWarning
+            validate_on_submit=True 
         )
-        reddit.user.me()  # Check if authentication succeeded
+        reddit.user.me()
         return reddit
-    except Exception as e:
+    except praw.exceptions.APIException as e:
         print("Error: Could not authenticate with the provided credentials.")
         exit()
 
 def delete_old_comments(reddit, username, days_old, comments_deleted):
+    """
+    Delete comments older than a specified number of days.
+
+    Args:
+        reddit (praw.Reddit): Authenticated Reddit instance.
+        username (str): Reddit username.
+        days_old (int): Age limit for comments (in days).
+        comments_deleted (list): A list to store deleted comments.
+
+    Notes:
+        This function will delete comments older than the specified age limit.
+    """
     for comment in reddit.redditor(username).comments.new(limit=None):
         if time.time() - comment.created_utc > days_old * 24 * 60 * 60:
             with open('deleted_comments.txt', 'a') as f:
                 f.write(comment.body + '\n')
-            comment.edit(".")
-            comment.delete()
-            comments_deleted.append(comment)
+            try:
+                comment.edit(".")
+                comment.delete()
+                comments_deleted.append(comment)
+            except praw.exceptions.APIException as e:
+                print(f"Error deleting comment: {e}")
 
 def remove_comments_with_negative_karma(reddit, username, comments_deleted):
+    """
+    Remove comments with negative karma.
+
+    Args:
+        reddit (praw.Reddit): Authenticated Reddit instance.
+        username (str): Reddit username.
+        comments_deleted (list): A list to store deleted comments.
+
+    Notes:
+        This function will remove comments with a negative karma score.
+    """
     for comment in reddit.redditor(username).comments.new(limit=None):
         if comment.score < 0:
             with open('deleted_comments.txt', 'a') as f:
                 f.write(comment.body + '\n')
-            comment.edit(".")
-            comment.delete()
-            comments_deleted.append(comment)
+            try:
+                comment.edit(".")
+                comment.delete()
+                comments_deleted.append(comment)
+            except praw.exceptions.APIException as e:
+                print(f"Error removing comment: {e}")
 
 def main():
-
     client_id, client_secret, username, password = get_reddit_credentials()
 
     if not confirm_and_run():
